12bea0d1dc4e3a4aca5a84f90dd246df657cc4a1
Marco Impellizzeri <impellizzeri.marco18@gmail.com>
2025-07-28 17:24:00 +0200

    estrazione e riutilizzo di Input e Button in AuthInputs tramite styled-components

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components'
+
+const Button = styled.button`
+    padding: 0.75rem 1rem;
+    line-height: 1.5;
+    background-color: #f0b322;
+    color: #1f2937;
+    border: 1px solid transparent;
+
+    &:hover {
+        background-color: #f0920e;
+    }
+`
+
+export default Button;
